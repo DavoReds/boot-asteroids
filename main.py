0c100375c66c66b2cfc0f3ecf0262c6b46aa7987
Dalia Rojas Vallejo <daliarojasvallejo@gmail.com>
2026-04-07 02:01:13 +0000

    Put asteroids on the screen
    FossilOrigin-Name: 6f6dd29243fa412271885c74b7f3908f9ace0fed4dbca6eaec8e8ee2aa894ad7

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-from player import Player
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    field = AsteroidField()
+
+    dt = 0
 
     while True:
         log_state()
@@ -26,11 +35,14 @@ def main():
                 return
 
         # Updating
-        player.update(dt)
+        updatable.update(dt)
 
         # Rendering
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # Ticking
